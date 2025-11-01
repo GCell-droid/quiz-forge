340d05a85fd0c910180de071ae6d4d180c519a34
GCell-droid <niteshparihar104@gmail.com>
2025-11-02 03:21:32 +0530

    Remove dist and test folders from repository and add to .gitignore

--- dist/auth/dto/register.dto.d.ts
+++
@@ -1,7 +0,0 @@
-import { UserRole } from '../entity/user.entity';
-export declare class RegisterDTO {
-    email: string;
-    name: string;
-    password: string;
-    role: UserRole;
-}
--- dist/quiz/quiz.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { QuizService } from './quiz.service';
-import { CreateQuizDto } from './dto/create-quiz-dto';
-export declare class QuizController {
-    private readonly quizService;
-    constructor(quizService: QuizService);
-    CreateQuizDto(req: Request & {
-        user?: any;
-    }, quizz: CreateQuizDto): Promise<import("./entites/quiz.entity").QuizEntity>;
-}
