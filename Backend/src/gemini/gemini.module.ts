3337bff60fb4c28171e23b75468ff66d17109664
GCell-droid <niteshparihar104@gmail.com>
2026-05-18 00:49:01 +0530

    Remove backend folder from frontend

--- Backend/src/gemini/gemini.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { GeminiController } from './presenters/http/gemini.controller';
-import { GeminiService } from './application/gemini.service';
-
-@Module({
-  providers: [GeminiService],
-  controllers: [GeminiController],
-})
-export class GeminiModule {}
