6fb7814424805ad6eb6298346aae54c06064eb43
GCell-droid <niteshparihar104@gmail.com>
2026-07-14 23:24:42 +0530

    For Prod

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -4,8 +4,11 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
 
+import { NestExpressApplication } from '@nestjs/platform-express';
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.set('trust proxy', 1);
   const frontendUrl = process.env.FRONTEND_URL;
 
   if (!frontendUrl) {
